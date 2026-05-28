f7f18ff4047605ff875d2a0039f188cfe3385116
PedroHenriquesDev <133235396+PedroHenriquesDev@users.noreply.github.com>
2026-05-28 14:35:32 -0300

    Add files via upload

---
+++ Dicionário/ex090.py
@@ -0,0 +1,11 @@
+aluno = {}
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Média de {aluno['nome']}:'))
+
+if aluno['media'] >= 6.0:
+    aluno['situação'] = 'Aprovado'
+else:
+    aluno['situação'] = 'Reprovado'
+
+for k,v in aluno.items():
+    print(f' - {k} é igual a {v}')
