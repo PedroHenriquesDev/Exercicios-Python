8515543a9cec9a2e5b82b79cda270e42f82572e2
PedroHenriquesDev <133235396+PedroHenriquesDev@users.noreply.github.com>
2026-09-10 20:11:46 -0300

    Delete Tupla directory

--- Tupla/ex077.py
+++
@@ -1,8 +0,0 @@
-palavras = ('aprender','programar','linguagem','python',
-            'curso','gratis','estudar','praticar','trabalhar',
-            'mercado','programador','futuro')
-for p in palavras:
-    print(f'Na palavra {p} temos ')
-    for letra in p:
-        if letra.lower() in 'aeiou':
-            print(letra,end=' ')
