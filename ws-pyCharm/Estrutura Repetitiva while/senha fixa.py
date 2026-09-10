0499fd8692e5a3b7e6fcb6d6a7f3f2c26168430f
PedroHenriquesDev <133235396+PedroHenriquesDev@users.noreply.github.com>
2026-09-10 20:11:55 -0300

    Delete ws-pyCharm directory

--- ws-pyCharm/Estrutura Repetitiva while/senha fixa.py
+++
@@ -1,9 +0,0 @@
-senha:int
-
-senha = int(input('Digite a senha:'))
-
-while senha != 2002:
-    senha = int(input('Senha inválida! Tente novamente:'))
-
-    if senha == 2002:
-        print('Acesso permitido! ')
--- ws-pyCharm/Estrutura Sequencial/duracao.py
+++
@@ -1,9 +0,0 @@
-duracao = int(input("Digite a duracao em segundos: "))
-
-horas = duracao // 3600
-resto = duracao % 3600
-
-minutos = resto // 60
-segundos = resto % 60
-
-print(f"{horas}:{minutos}:{segundos}")
